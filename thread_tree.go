804ba1c4140f2d3feac8c97d6e0f02381333740c
marsdong <marsdong@tencent.com>
2022-10-10 17:26:08 +0800

    bug fix

--- thread_tree.go
+++ thread_tree.go
@@ -2,6 +2,10 @@
 // MarsDong 2022/10/10
 package host_service_discovery
 
+import (
+	"fmt"
+)
+
 type ITree interface {
 	Traverse() Nodes
 }
@@ -29,6 +33,14 @@ func (n Nodes) GetIDs() []int32 {
 	return ret
 }
 
+func (n Nodes) Set() map[int32]*Node {
+	ret := make(map[int32]*Node)
+	for _, node := range n {
+		ret[node.ID] = node
+	}
+	return ret
+}
+
 func NewNode(id, parent int32) *Node {
 	return &Node{
 		ID:       id,
@@ -45,11 +57,13 @@ func NewTree(roots Nodes, nodes Nodes) *Tree {
 
 	rootSet := make(map[int32]struct{})
 	for _, root := range roots {
-		tree.Roots = append(tree.Roots, root)
-		tree.Nodes[root.ID] = root
+		rootNode := NewNode(root.ID, root.Parent)
+		tree.Roots = append(tree.Roots, rootNode)
+		tree.Nodes[root.ID] = rootNode
 		rootSet[root.ID] = struct{}{}
 	}
 
+	nodeSet := nodes.Set()
 	for _, node := range nodes {
 		if _, exists := rootSet[node.ID]; exists {
 			continue
@@ -58,7 +72,11 @@ func NewTree(roots Nodes, nodes Nodes) *Tree {
 			tree.Nodes[node.ID] = NewNode(node.ID, node.Parent)
 		}
 		if _, exists := tree.Nodes[node.Parent]; !exists {
-			tree.Nodes[node.Parent] = NewNode(node.ID, node.Parent)
+			parentNode, exists := nodeSet[node.Parent]
+			if !exists {
+				continue
+			}
+			tree.Nodes[parentNode.ID] = NewNode(parentNode.ID, parentNode.Parent)
 		}
 		tree.Nodes[node.Parent].Children = append(tree.Nodes[node.Parent].Children, tree.Nodes[node.ID])
 	}
@@ -68,7 +86,13 @@ func NewTree(roots Nodes, nodes Nodes) *Tree {
 func (t *Tree) Traverse() Nodes {
 	ret := make(Nodes, 0)
 	for _, root := range t.Roots {
-		ret = append(ret, t.traverseByRoot(root)...)
+		nodes := t.traverseByRoot(root)
+		ret = append(ret, nodes...)
+		debug := make([]int32, 0)
+		for _, node := range nodes {
+			debug = append(debug, node.ID)
+		}
+		fmt.Println(debug)
 	}
 	return ret
 }
@@ -79,9 +103,10 @@ func (t *Tree) traverseByRoot(root *Node) (children Nodes) {
 		return children
 	}
 	for _, node := range root.Children {
-		for _, n := range t.traverseByRoot(node) {
-			children = append(children, n)
-		}
+		//for _, n := range t.traverseByRoot(node) {
+		//	children = append(children, n)
+		//}
+		children = append(children, t.traverseByRoot(node)...)
 
 	}
 	return children
